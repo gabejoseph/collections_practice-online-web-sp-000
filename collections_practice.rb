c83cab575b3f4610ff22b35dc83ad27406135365
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-07 20:04:19 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,5 +35,5 @@ def sum_array(x)
 end
 
 def add_s(x)
-  x.each_with_index.collect{|element, index| element << "s"}
+  x.each_with_index.collect{|element, index| element << "s" yield index[1] = "feet"}
 end
